5388f035ff1b5c1101ce6d4c435ba542a236f9f7
manal <manal.ahmadi@uir.ac.ma>
2024-03-26 14:44:12 -0500

    replaced images with videos

--- src/Components/ScrollPage.js
+++ src/Components/ScrollPage.js
@@ -10,10 +10,31 @@ const ScrollPage = ({ event, goToNextEvent, goToPreviousEvent }) => {
   const onClick=()=>{
     navigate('/EventDetailsMobile')
   }
+
+
+  // Inside your component
+React.useEffect(() => {
+  const video = document.querySelector('video');
+
+  if (video) {
+    video.play().catch(error => console.error("Video play failed.", error));
+  }
+}, []);
+
   return (
     <div className='scrollPage'>
       <div className='videoFrame'>
-        <img src={event.imageUrl} alt={event.name} style={{ width: '100%'}} />
+        {/* <img src={event.imageUrl} alt={event.name} style={{ width: '100%'}} /> */}
+        <video 
+          src={event.videoUrl} 
+          alt={event.name} 
+          style={{ width: '100%', height: 'auto' }} 
+          autoplay 
+          playsInline
+          muted
+          loop
+          controls
+        />
         <div className='video-content'>
           <h1>{event.name}</h1>
           <p>Join us for an unforgettable night at Pita Pit</p>
